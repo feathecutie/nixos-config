929f68eb27ea1fed9a2a3ae3c7894e4e0d6fca19
Fea <feathecutie@mail.de>
2022-08-17 15:38:04 +0200

    PONIES!!

--- modules/programs/shell/nushell.nix
+++ modules/programs/shell/nushell.nix
@@ -1,12 +1,18 @@
 {
-  users.fea.programs.nushell = {
-    enable = true;
-    envFile.text = ''
-      mkdir ~/.cache/starship
-      starship init nu | save ~/.cache/starship/init.nu
-    '';
-    configFile.text = ''
-      source ~/.cache/starship/init.nu
-    '';
-  };
+  users.fea =
+    { pkgs
+    , ...
+    }: {
+      programs.nushell = {
+        enable = true;
+        envFile.text = ''
+          mkdir ~/.cache/starship
+          starship init nu | save ~/.cache/starship/init.nu
+        '';
+        configFile.text = ''
+          source ~/.cache/starship/init.nu
+          ${pkgs.ponysay}/bin/ponysay --ponyonly
+        '';
+      };
+    };
 }
