3015d67eeea7ca39f18a939fdf047ea855b13dd9
Fea <feathecutie@mail.de>
2022-08-30 23:02:22 +0200

    fixed skyrim together

--- hosts/server/skyrim-together.nix
+++ hosts/server/skyrim-together.nix
@@ -8,8 +8,8 @@
         image = "tiltedphoques/st-reborn-server:latest";
         ports = [ "10578:10578/udp" ];
         volumes = [
-          "/home/fea/skyrim-together/config:/home/server/config",
-          "/home/fea/skyrim-together/logs:/home/server/logs",
+          "/home/fea/skyrim-together/config:/home/server/config"
+          "/home/fea/skyrim-together/logs:/home/server/logs"
           "/home/fea/skyrim-together/Data:/home/server/Data"
         ];
       };
