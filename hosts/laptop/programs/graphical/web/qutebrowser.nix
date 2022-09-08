8454bdd0575134eb0f9b51b9cdef7b0f65966e16
Fea <feathecutie@mail.de>
2022-09-08 17:24:30 +0200

    Configured qutebrowser to use Startpage

--- hosts/laptop/programs/graphical/web/qutebrowser.nix
+++ hosts/laptop/programs/graphical/web/qutebrowser.nix
@@ -1,3 +1,14 @@
 {
-  users.fea.programs.qutebrowser.enable = true;
+  users.fea.programs.qutebrowser = 
+  let
+    startpageUrl = "https://www.startpage.com";
+  in
+  {
+    enable = true;
+    searchEngines.DEFAULT = "${startpageUrl}/sp/search?query={}";
+    settings.url = {
+      default_page = startpageUrl;
+      start_pages = startpageUrl;
+    };
+  };
 }
