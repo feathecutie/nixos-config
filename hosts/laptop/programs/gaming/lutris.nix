03acea98297dc3653d8ecc6142b2c33a5a408986
Fea <feathecutie@mail.de>
2022-09-10 16:27:23 +0200

    laptop: Temporarily update lutris to unmerged PR to fix EGS

--- hosts/laptop/programs/gaming/lutris.nix
+++ hosts/laptop/programs/gaming/lutris.nix
@@ -2,7 +2,19 @@
   users.fea =
     { pkgs
     , ...
-    }: {
-      home.packages = [ pkgs.lutris ];
+    }:
+    let
+      lutris-unwrapped = pkgs.lutris-unwrapped.overrideAttrs (previousAttrs: {
+        src = pkgs.fetchFromGitHub {
+          owner = "lutris";
+          repo = "lutris";
+          rev = "d350cd7d29c79acaeddfa846dbd118c5385141a4";
+          sha256 = "sha256-Mo/2sxFUC7tRZBZXKS19q1VnSfRaNIy3fdvvJPR/Tfo=";
+        };
+        propagatedBuildInputs = previousAttrs.propagatedBuildInputs ++ [ pkgs.python310Packages.pypresence ];
+      });
+    in
+    {
+      home.packages = [ (pkgs.lutris.override { inherit lutris-unwrapped; }) ];
     };
 }
